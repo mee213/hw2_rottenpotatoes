3ad8e871abdaa02e30fa9b48304862e2c5e2eb34
mee213 <evans.marlo@gmail.com>
2013-04-07 15:01:28 -0700

    HW2_3 v.4

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,48 +7,43 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings = Movie.all_ratings
 
     needs_redirect = true
 
-    if params[:order] && params[:ratings]
+    if params[:order] != session[:order] && params[:ratings] != session[:order]
       needs_redirect = false
     end
 
-    @all_ratings = Movie.all_ratings
-
     if params[:order]
       session[:order] = params[:order]
-    end
-  
-    @order = session[:order]
-    
-    if @order == "title"
-        @title_class = "hilite"
-    elsif @order == "release_date"
-        @date_class = "hilite"
     end
 
     if params[:ratings]
       session[:ratings] = params[:ratings]
     end
 
-    if session[:ratings]
-      ratings = session[:ratings]
-      if ratings.kind_of?(Array)
-        @ratings = ratings
-      else
-        @ratings = ratings.keys
-      end
-    else
-      @ratings = @all_ratings
+    if session[:order] == "title"
+        @title_class = "hilite"
+    elsif session[:order] == "release_date"
+        @date_class = "hilite"
     end
 
-    @movies = Movie.find(:all, :conditions => ['rating IN (?)', @ratings], :order => @order)
+    if session[:ratings]
+      if session[:ratings].kind_of?(Array)
+        session[:ratings] = session[:ratings]
+      else
+        session[:ratings] = session[:ratings].keys
+      end
+    else
+      session[:ratings] = @all_ratings
+    end
+
+    @movies = Movie.find(:all, :conditions => ['rating IN (?)', session[:ratings]], :order => session[:order])
 
     if needs_redirect
       flash.keep
-      redirect_to movies_path(:order => @order, :ratings => @ratings)
-      session.clear
+      redirect_to movies_path(:ratings => session[:ratings], :order => session[:order])
     end
   end
 
